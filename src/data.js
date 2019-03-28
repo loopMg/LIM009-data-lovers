bd1e046be76fb1a49ed91a3c2e622db314716a36
holi <asadasd@ajsdjsad.xom>
2019-03-27 23:12:48 -0500

    first full user story

--- src/data.js
+++ src/data.js
@@ -1,34 +1,38 @@
-const dataPokemon = POKEMON.pokemon;
+//Para acceder al array de objetos con todos los pokemones
+const dataPokemon = POKEMON.pokemon; 
+/* Codigo que se ve, se copio y no se entiende nunca se aprende*/
 
 // console.log(dataPokemon);
 
 /* Bucle p/recorrer todo el array de POKEMON */  
 // let arrPokemons = [];
- for (let i=0; i<dataPokemon.length; i++) {
-   console.log(dataPokemon[i])
+ //for (let i=0; i<dataPokemon.length; i++) {
+   //console.log(dataPokemon[i])
 //   arrPokemons += dataPokemon[i];
-  }
+  //}
 //   console.log(arrPokemons);
 
  /*filtrar data por parametro*/
-  const filteredPokemons = (data, string) => {
-    return data.filter(obj=> obj.type.includes(string));
+  const filteredPokemons = (arrData, string) => {
+    return arrData.filter(object=> object.type.includes(string));
   }
 
-  const arrImp = filteredPokemons(dataPokemon ,'Psychic');
-  console.log(arrImp);
+  const arrFilterPokemons = filteredPokemons(dataPokemon ,'Fire');
+  // console.log(arrFilterPokemons);
 
 /*ordenar data de forma ascendente*/
-const sortedPokemons =(data)=>{
-  let var1=data.sort(function(a,b){
-    if(a.name > b.name) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
-  return var1;
-} ;
+// const sortedPokemons =(data)=>{
+//   let var1=data.sort(function(a,b){
+//     if(a.name > b.name) {
+//       return 1;
+//     } else {
+//       return -1;
+//    }
+//   });
+//  return var1;
+// } ;
+
+// const arrSortPokemons = sortedPokemons(dataPokemon);
 // console.log(sortedPokemons(dataPokemon));
 
 
--- src/main.js
+++ src/main.js
@@ -1,22 +1,43 @@
 /* Manejo del DOM */
 
-/* imprimir data en pantalla*/
-const addPokemon = (pokemones,ruta) => {
-  const root = document.getElementById(ruta);
-    for (let i = 0; i < pokemones.length; i++) {
-      root.innerHTML += `
-        <div> 
-          <figure>
-            <img src="${pokemones[i].img}" alt="perfil" class="img-profile">
-          </figure>
-          <p>Number:${pokemones[i].num}</p>
-          <h2>Nombre: ${pokemones[i].name}</h2>
-          <p>Tipo: ${pokemones[i].type}</p>
-          
-      </div>
-    `; 
-  }
-  }
-  // addPokemon(sortedPokemons(dataPokemon),'root')
-  addPokemon(arrImp,'root');
-  // addPokemon(dataPokemon,'root');
+// botones
+// const buttomFilter = document.getElementById("btm-filter");
+// const buttomTypeWater = document.getElementById("btm-Water");
+// Eventos de Botones
+
+// buttomTypeWater.addEventListener('click')=> {
+//   const 
+// }
+const boxLine = document.getElementById("pokemons-view");
+
+const printPokemons = (arrData, contenedorHTML) => {
+let string = '';
+for(let i = 0; i<arrData.length; i++) {
+  string += `<div id="poke-box">
+                  <img src="${arrData[i].img}" alt="pokemon" class="img-poke"/>
+                  <p>${arrData[i].name}</p>
+                  <p>${arrData[i].type}</p> </div>
+ `
+}
+
+contenedorHTML.innerHTML = string; 
+}
+
+
+printPokemons(dataPokemon, boxLine)
+// printPokemons(arrFilterPokemons,boxLine)
+// printPokemons(arrSortPokemons,boxLine);
+
+
+
+/* Esto funciona, me siento cool !!! 
+let elementPoke = '';
+for(let i = 0; i<dataPokemon.length; i++) {
+  elementPoke += `<div>
+                  <img src="${dataPokemon[i].img}" alt="pokemon" class="img-poke"/>
+                  <p>${dataPokemon[i].name}</p>
+                  <p>${dataPokemon[i].type}</p> </div>
+ `
+}
+imp.innerHTML = elementPoke;
+*/
